9a08df55a82f29440cce8fca4eb5427b58bc0a6d
Osmar Saraiva Jr <osmar.saraiva@gmail.com>
2024-02-27 22:36:41 -0300

    Commit inicial projeto

---
+++ src/main/java/br/com/loja_virtual/LojaVirtualJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.loja_virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/loja_virtual/LojaVirtualJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.loja_virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
